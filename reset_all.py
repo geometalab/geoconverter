5ce093f476036fd997d7c52d607eadf111508478
Marcel Huber <marcel.huber@hsr.ch>
2014-06-11 16:10:30 +0200

    removed stored sqlite files, regenerate with reset_all.py
    added some exception safety to reset_all.py

--- reset_all.py
+++ reset_all.py
@@ -1,51 +1,39 @@
-import os, shutil
+#!/usr/bin/env python
+
+import os, sys, shutil
+
+dbname_option_tuples = [
+    ('default.sqlite', ''),
+    ('sessions.sqlite', '--database=sessions_db'),
+    ('OGRgeoConverter/ogrgeoconverter.sqlite', '--database=ogrgeoconverter_db'),
+    ('OGRgeoConverter/log.sqlite', '--database=ogrgeoconverter_log_db'),
+    ('OGRgeoConverter/conversionjobs.sqlite', '--database=ogrgeoconverter_conversion_jobs_db'),
+]
 
 print('Warning: All data will be lost!')
-answer = input('Do you want to continue? (yes/no): ')
+answer = input('Do you want to continue with cleanup? (yes/no): ')
 if answer == 'yes':
-    print('')
-    
-    print('Removing old conversion files ...')
-    folder_path = './OGRgeoConverter/ConversionJobFiles'
-    for folder in os.listdir(folder_path):
-        full_path = os.path.join(folder_path, folder)
-        shutil.rmtree(full_path)
-    
-    print('Removing database.sqlite ...')
-    if os.path.exists('default.sqlite'):
-        os.remove('default.sqlite')
-        
-    print('Removing sessions.sqlite ...')
-    if os.path.exists('sessions.sqlite'):
-        os.remove('sessions.sqlite')
-        
-    print('Removing ogrgeoconverter.sqlite ...')
-    if os.path.exists('./OGRgeoConverter/ogrgeoconverter.sqlite'):
-        os.remove('./OGRgeoConverter/ogrgeoconverter.sqlite')
-        
-    print('Removing log.sqlite ...')
-    if os.path.exists('./OGRgeoConverter/log.sqlite'):
-        os.remove('./OGRgeoConverter/log.sqlite')
-    
-    print('Removing conversionjobs.sqlite ...')
-    if os.path.exists('./OGRgeoConverter/conversionjobs.sqlite'):
-        os.remove('./OGRgeoConverter/conversionjobs.sqlite')
-            
-    print('Creating default.sqlite ...')
-    os.system('python manage.py syncdb')
+    print('\nRemoving old conversion files ...')
+    try:
+        folder_path = './OGRgeoConverter/ConversionJobFiles'
+        for folder in os.listdir(folder_path):
+            full_path = os.path.join(folder_path, folder)
+            shutil.rmtree(full_path)
+    except: pass
 
-    print('Creating sessions.sqlite ...')
-    os.system('python manage.py syncdb --database=sessions_db')
-    
-    print('Creating ogrgeoconverter.sqlite ...')
-    os.system('python manage.py syncdb --database=ogrgeoconverter_db')
-    
-    print('Creating log.sqlite ...')
-    os.system('python manage.py syncdb --database=ogrgeoconverter_log_db')
-    
-    print('Creating conversionjobs.sqlite ...')
-    os.system('python manage.py syncdb --database=ogrgeoconverter_conversion_jobs_db')
-    
+    for dbname,option in dbname_option_tuples:
+        print('Removing', dbname, '...')
+        if os.path.exists(dbname):
+            os.remove(dbname)
+ 
+print('\nSetup new environment:')
+answer = input('Do you want to setup now? (yes/no): ')
+if answer == 'yes':
+    print('\n')
+    for dbname,option in dbname_option_tuples:
+        print('Creating', dbname, '...')
+        os.system(' '.join(['python manage.py syncdb',option]))
+ 
     print('Loading fixture ogr_formats.json ...')
     os.system('python manage.py loaddata ogr_formats.json --database=ogrgeoconverter_db')
 
