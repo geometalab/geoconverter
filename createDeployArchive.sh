ac22f8ae5e2d8256791d33b3ac4101e2b9395de8
Marcel Huber <marcel.huber@hsr.ch>
2014-06-11 17:20:47 +0200

    extended date by time

--- createDeployArchive.sh
+++ createDeployArchive.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git archive --prefix=geoconverter`date +-%Y-%m-%d`/ HEAD | gzip > geoconverter.`date +%Y%m%d`.tar.gz
+git archive --prefix=geoconverter`date +-%Y-%m-%d-%H%M%S`/ HEAD | gzip > geoconverter.`date +%Y%m%d%H%M%S`.tar.gz
 
